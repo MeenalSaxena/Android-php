c73966e271c6e00abe91e5087f1d846cf14ccf00
mayankmandloi <mayankmandloi009@gmail.com>
2018-03-16 10:48:52 +0530

    DB

--- recipe_database/recipe.sql
+++ recipe_database/recipe.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.7.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 14, 2018 at 09:23 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.2
+-- Host: localhost
+-- Generation Time: Mar 16, 2018 at 10:48 AM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.25-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,6 +35,14 @@ CREATE TABLE `ingredients_table` (
   `quantity` int(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `ingredients_table`
+--
+
+INSERT INTO `ingredients_table` (`ingredient_id`, `recipe_id`, `ingredient_name`, `quantity`) VALUES
+(1, 1, 'Maggie', 1),
+(2, 1, 'tamato', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +55,16 @@ CREATE TABLE `recipe_steps` (
   `step_number` int(20) NOT NULL,
   `step_description` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recipe_steps`
+--
+
+INSERT INTO `recipe_steps` (`step_id`, `recipe_id`, `step_number`, `step_description`) VALUES
+(1, 1, 1, 'chop tomato and carrot cabbage in equal portion.'),
+(2, 1, 2, 'put a pan on gas and pour 250 ml of water in it.'),
+(3, 1, 3, 'heat for 2 mins and boil the water.'),
+(4, 1, 4, 'put Maggie and maggie masala.it it \r\nboil for 2 mins  ');
 
 -- --------------------------------------------------------
 
@@ -63,6 +81,13 @@ CREATE TABLE `recipe_table` (
   `cook_time` time(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `recipe_table`
+--
+
+INSERT INTO `recipe_table` (`recipe_id`, `u_id`, `recipe_name`, `recipe_description`, `prep_time`, `cook_time`) VALUES
+(1, 1, 'maggie', '2 min food', '00:10:00.000000', '00:10:00.000000');
+
 -- --------------------------------------------------------
 
 --
@@ -75,6 +100,13 @@ CREATE TABLE `user_table` (
   `u_email` varchar(20) NOT NULL,
   `u_password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`u_id`, `u_name`, `u_email`, `u_password`) VALUES
+(1, 'dummy', 'd@d.com', '123456');
 
 --
 -- Indexes for dumped tables
@@ -115,26 +147,22 @@ ALTER TABLE `user_table`
 -- AUTO_INCREMENT for table `ingredients_table`
 --
 ALTER TABLE `ingredients_table`
-  MODIFY `ingredient_id` int(20) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `ingredient_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `recipe_steps`
 --
 ALTER TABLE `recipe_steps`
-  MODIFY `step_id` int(20) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `step_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `recipe_table`
 --
 ALTER TABLE `recipe_table`
-  MODIFY `recipe_id` int(20) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `recipe_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `user_table`
 --
 ALTER TABLE `user_table`
-  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
