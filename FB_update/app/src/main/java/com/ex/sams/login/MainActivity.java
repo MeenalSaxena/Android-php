24c2d8f9f8a7df5ed2434ed19c774cc881a8f739
KanupriyaVerma <pkanu95@gmail.com>
2018-04-16 10:18:27 +0530

    Finished FB_update Database Connection

--- FB_update/app/src/main/java/com/ex/sams/login/MainActivity.java
+++ FB_update/app/src/main/java/com/ex/sams/login/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ex.sams.login;
         import android.os.AsyncTask;
+        import android.os.Build;
         import android.os.Bundle;
-        import android.os.Parcelable;
+        import android.support.annotation.RequiresApi;
+        import android.util.Log;
         import android.webkit.WebView;
         import android.widget.ImageView;
         import android.widget.TextView;
@@ -9,8 +11,16 @@ package com.ex.sams.login;
         import android.support.v7.app.AppCompatActivity;
         import android.widget.Toast;
 
+        import org.apache.http.HttpEntity;
+        import org.apache.http.HttpResponse;
         import org.apache.http.NameValuePair;
+        import org.apache.http.client.HttpClient;
+        import org.apache.http.client.entity.UrlEncodedFormEntity;
+        import org.apache.http.client.methods.HttpPost;
+        import org.apache.http.impl.client.DefaultHttpClient;
         import org.apache.http.message.BasicNameValuePair;
+        import org.apache.http.protocol.HTTP;
+        import org.json.JSONArray;
         import org.json.JSONException;
         import org.json.JSONObject;
         import com.facebook.AccessTokenTracker;
@@ -27,8 +37,12 @@ package com.ex.sams.login;
         import com.facebook.GraphResponse;
         import com.facebook.appevents.AppEventsLogger;
 
-        import java.io.Serializable;
+        import java.io.BufferedReader;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.io.UnsupportedEncodingException;
         import java.util.ArrayList;
+        import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,17 +54,18 @@ public class MainActivity extends AppCompatActivity {
     private AccessTokenTracker accessTokenTracker;
     private ProfileTracker profileTracker;
     LoginButton loginButton;
+    String name, first_name, last_name, gender;
     ImageView imageView;
     String  email;
     GraphRequest graphRequest;
     WebView display;
     FacebookCallback<LoginResult> callback;
     String URL="http://10.0.2.2/data.php";
-
+    JSONArray jsonArray = new JSONArray();
+    String check;
     Bundle bundle = new Bundle();
-    JSONParser jsonParser=new JSONParser();
 
-
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,12 +77,21 @@ public class MainActivity extends AppCompatActivity {
         callbackManager= CallbackManager.Factory.create();
         display=(WebView)findViewById(R.id.webView);
         loginButton= (LoginButton)findViewById(R.id.loginbutton);
+        display.loadUrl(URL);
+
+        Log.w("tag", "********************************************************************************");
+
 
 
         loginButton.registerCallback(callbackManager, callback);
         loginButton.setReadPermissions("user_friends");
         if (AccessToken.getCurrentAccessToken() != null) {
-            GraphLoginRequest(AccessToken.getCurrentAccessToken());
+            try {
+                GraphLoginRequest(AccessToken.getCurrentAccessToken());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
 
             // If already login in then show the Toast.
             Toast.makeText(MainActivity.this, "Already logged in", Toast.LENGTH_SHORT).show();
@@ -81,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Adding Click listener to Facebook login button.
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>(){
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onSuccess(LoginResult loginResult){
                 AccessToken accessToken;
@@ -88,8 +113,13 @@ public class MainActivity extends AppCompatActivity {
                 Profile profile;
                 profile= Profile.getCurrentProfile();
 
+
                 // Calling method to access User Data After successfully login.
-                GraphLoginRequest(loginResult.getAccessToken());
+                try {
+                    GraphLoginRequest(loginResult.getAccessToken());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -131,15 +161,20 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+
+
     // Method to access Facebook User Data.
-    protected Bundle GraphLoginRequest(AccessToken accessToken){
+    protected String GraphLoginRequest(AccessToken accessToken) throws JSONException {
         graphRequest = GraphRequest.newMeRequest(accessToken,
                 new GraphRequest.GraphJSONObjectCallback() {
+
+
                     @Override
-                    public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+                    public void onCompleted( JSONObject jsonObject, GraphResponse graphResponse) {
 
                         try {
-
+//                            System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+//                            System.out.println(jsonObject);
                             // Adding all user info one by one into TextView.
                             textView.setText("ID: " + jsonObject.getString("id"));
 
@@ -162,6 +197,7 @@ public class MainActivity extends AppCompatActivity {
                             textView.setText(textView.getText() + "\nVerified : " + jsonObject.getString("verified"));
 
 
+//
                             //textView.setText(textView.getText() + "\nEmail : " + jsonObject.getString("email"));
 
 
@@ -169,13 +205,30 @@ public class MainActivity extends AppCompatActivity {
                         catch (JSONException e) {
                             e.printStackTrace();
                         }
+
+
+
+                        check = jsonObject.toString();
+                        System.out.println("Hello i am here"+check);
+
+                        AttemptLogin attemptLogin= new AttemptLogin();
+
+
+                        attemptLogin.execute(check,"","");
+
+                      //  System.out.println(jsonObject);
+
+
+
+
                     }
                 });
 
         bundle.putString("fields", "id,name,link,email,gender,last_name,first_name,locale,timezone,updated_time,verified");
         graphRequest.setParameters(bundle);
         graphRequest.executeAsync();
-        return bundle;
+
+        return null;
 
     }
 
@@ -199,81 +252,149 @@ public class MainActivity extends AppCompatActivity {
         AppEventsLogger.deactivateApp(MainActivity.this);
 
     }
-    private abstract class AttemptLogin extends AsyncTask<Bundle, Void, Void> {
+
+        class AttemptLogin extends AsyncTask<String, String, String> {
 
 
 
-        @Override
 
-        protected void onPreExecute() {
+            @Override
 
-            super.onPreExecute();
+            protected void onPreExecute() {
 
-        }
+                super.onPreExecute();
+                System.out.println("$$$$$$$$$$$$$$$");
 
 
-        protected JSONObject doInBackground(String... args) {
 
-            String name= bundle.getString(args[1]);
-            String first_name= bundle.getString(args[2]);
-            String last_name= bundle.getString(args[3]);
-            String gender= bundle.getString(args[4]);
-            ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("name", name));
-            params.add(new BasicNameValuePair("first_name", first_name));
-            params.add(new BasicNameValuePair("last_name", last_name));
-            params.add(new BasicNameValuePair("gender", gender));
-            JSONObject js = JSONParser.makeHttpRequest(URL, "POST",params );
-            Intent in=new Intent(MainActivity.this, JSONParser.class);
-            in.putExtra("Json", (Parcelable) js);
-            return js;
-        }
-//        protected JSONObject doInBackground(String... args) {
+
+            }
+
+//            @Override
+//            protected String doInBackground(Bundle... bundles) {
 //
-////            Bundle bundle = new Bundle();
-////            bundle.putString("fields", "id,name,link,email,gender,last_name,first_name,locale,timezone,updated_time,verified");
-////            graphRequest.setParameters(bundle);
-////            graphRequest.executeAsync();
+//                HttpPost httpPost = new HttpPost(URL);
+//                char[] arr = bundle.getCharArray("name");
 //
-//            String name= args[1];
-//            String first_name= args[2];
-//            String last_name= args[3];
-//            String gender= args[4];
+////                first_name = bundle.getString("fields", "2");
+////                last_name = bundle.getString("fields", "3");
+////                gender = bundle.getString("fields", "4");
+//                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+////                nameValuePairs.add(new BasicNameValuePair("name", name));
+////                nameValuePairs.add(new BasicNameValuePair("first_name", first_name));
+////                nameValuePairs.add(new BasicNameValuePair("last_name", last_name));
+////                nameValuePairs.add(new BasicNameValuePair("gender", gender));
 //
-//            ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-//            params.add(new BasicNameValuePair("name", name));
-//            params.add(new BasicNameValuePair("first_name", first_name));
-//            params.add(new BasicNameValuePair("last_name", last_name));
-//            params.add(new BasicNameValuePair("gender", gender));
+//                try {
+//                    JSONArray jsonArray = new JSONArray();
 //
+//                    JSONObject jsonObject = new JSONObject();
+//                    jsonObject.put("name", name);
+//                    jsonObject.put("first_name", first_name);
+//                    jsonObject.put("last_name", last_name);
+//                    jsonObject.put("gender", gender);
+//                    jsonArray.put(jsonObject);
+//                    nameValuePairs.add(new BasicNameValuePair("arr", String.valueOf(jsonArray.toString())));
 //
-////            JSONObject ob = new JSONObject();
-//            JSONObject js = JSONParser.makeHttpRequest(URL, "POST", params);
-//            return js;
-//
-//        }
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+                @Override
+                protected String doInBackground(String... strings) {
 
-        protected void onPostExecute(JSONObject result) {
+//                String first_name = strings[2];
+//                String last_name = strings[3];
+//                String gender=strings[4];
+                HttpPost httpPost = new HttpPost(URL);
 
-            // dismiss the dialog once product deleted
-            //Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+//                nameValuePairs.add(new BasicNameValuePair("name", name));
+//                nameValuePairs.add(new BasicNameValuePair("first_name", first_name));
+//                nameValuePairs.add(new BasicNameValuePair("last_name", last_name));
+//                nameValuePairs.add(new BasicNameValuePair("gender", gender));
 
-            try {
+                    try {
+//                        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+//
+//                        System.out.println(check);
+//                    JSONObject jsonObject = new JSONObject(check);
+//                    jsonObject.put("name", name);
+//                    jsonObject.put("first_name", first_name);
+//                    jsonObject.put("last_name", last_name);
+//                    jsonObject.put("gender", gender);
+//                    jsonArray.put(jsonObject);
+//                    System.out.println("########################################");
+
+
+                        nameValuePairs.add(new BasicNameValuePair("arr", check));
+//                        System.out.println(nameValuePairs);
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                    try
+                    {
+                        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, HTTP.UTF_8));
+                    }
+                    catch (UnsupportedEncodingException e1)
+                    {
+
+                        e1.printStackTrace();
+                    }
+
+
+                    HttpClient client = new DefaultHttpClient();
+
+                    httpPost.setHeader("Accept", "application/json");
+                    httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+                    try
+                    {
+                        HttpResponse response = client.execute(httpPost);
+
+                        HttpEntity entity = response.getEntity();
+                        InputStream content = entity.getContent();
+                     //   System.out.println("Response"+response);
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                        String line;
+                        StringBuilder builder = new StringBuilder();
+                        while ((line = reader.readLine()) != null)
+                        {
+                            builder.append(line);
+                        }
+                        System.out.println("httpPost"+builder);
+                        if (builder.toString().length() > 0)
+                        {
+                            return " ";
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Log.e("tag", "Error insert valoracion");
+                        e.printStackTrace();
+                    }
+                    return "Error";
+                }
+
+
+
+            protected void onPostExecute(String result) {
+
+                // dismiss the dialog once product deleted
+                //Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+
                 if (result != null) {
-                    Toast.makeText(getApplicationContext(),result.getString("message"),Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),result.toString(),Toast.LENGTH_LONG).show();
 
                 } else {
                     Toast.makeText(getApplicationContext(), "Unable to retrieve any data from server", Toast.LENGTH_LONG).show();
                 }
-            } catch (JSONException e) {
-                e.printStackTrace();
+
+
             }
 
-
         }
-
-    }
-
-
-
 }
